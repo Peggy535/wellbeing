ca30756cb43c5de5b78cfa790f47a3200151f3a5
Wayne Pegg <peggy161152@gmail.com>
2020-07-28 19:45:09 +0100

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = config => {
+    config.addPassthroughCopy('./src/static/');
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+  };
---
+++ src/index.md
@@ -0,0 +1,10 @@
+---
+title: 'Hello, world'
+layout: 'layouts/home.html'
+canonical: 'https://www.justinehodgsonhypnotherapy.com'
+intro:
+    logoAlt: 'Well Being specialist | CBT, Yin Yoga Therapy, Hypnotherapy'
+    
+---
+
+Hello Well Being
