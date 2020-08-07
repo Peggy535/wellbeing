4a8503e859ac477f1c1c9b97949ef5319313bded
Wayne Pegg <peggy161152@gmail.com>
2020-08-07 16:41:59 +0100

    seo updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
-const schema = require("@quasibit/eleventy-plugin-schema");
 
 module.exports = config => {
-    config.addPlugin(schema);
     config.addPassthroughCopy('./src/static/');
     config.addPassthroughCopy("favicon.ico");
     config.addPassthroughCopy("admin");
