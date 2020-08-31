63c5d9659deaa3066649d04428a2496670bd1623
Wayne Pegg <peggy161152@gmail.com>
2020-08-31 21:11:01 +0100

    additionof slugify

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const slugify = require('slugify');
 
 module.exports = config => {
     // Universal slug filter strips unsafe chars from URLs
